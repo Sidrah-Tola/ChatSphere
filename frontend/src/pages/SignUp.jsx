0587c0b775efa809b71be15924128d00210a9e1d
Sidrah-Tola <sidrahtola@gmail.com>
2024-12-02 10:33:35 +0530

    Signup page UI

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,8 +1,71 @@
 import React from 'react'
+import GenderCheckbox from '../component/GenderCheckbox'
+import { Link } from "react-router-dom"
+
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+    <div className=" w-full p-6 rounded-lg shadow-md bg-gray-200">
+      <h1 className="text-3xl font-semibold text-center text-slate-700">
+        Signup to
+        <span className="text-blue-700"> ChatSphere</span>
+      </h1>
+
+      <form>
+      <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+
+            <input
+              type="text"
+              placeholder="Enter Username"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Email</span>
+            </label>
+
+            <input
+              type="email"
+              placeholder="abc123@gmail.com"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Pssword</span>
+            </label>
+
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+
+          <GenderCheckbox />
+
+          <Link to={"/login"} className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+          Already have an account?
+          </Link>
+
+          <div>
+            <button className="btn btn-block btn-sm mt-2 bg-blue-700 text-white">
+                Sign Up
+                </button>
+          </div>
+
+
+
+      </form>
+      </div>
+      </div>
   )
 }
 
